96a13bc51be25c10d8a54218137c933660cdc8df
edilson258 <dev.258.edilson@gmail.com>
2025-01-10 22:07:37 +0200

    doc: update example to reflet new changes

--- examples/parse_and_stringfy.c
+++ examples/parse_and_stringfy.c
@@ -33,8 +33,11 @@ int main()
   char *path = "test-file.json";
   char *content = read_file_to_buf(path);
 
-  jjson_t json = Json_Parse(content);
-  Json_Print(&json, 4);
+  jjson_t json;
+  jjson_init(&json);
+
+  jjson_parse(&json, content);
+  jjson_dump(&json, 4);
 
   // clean up
   free(content);
